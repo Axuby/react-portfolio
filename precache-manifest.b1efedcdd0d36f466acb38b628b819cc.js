215680387f99296fe2a530d017b6791082289f5d
Axuby <azubuinesamuel@gmail.com>
2021-09-26 03:20:14 +0100

    Updates

---
+++ precache-manifest.b1efedcdd0d36f466acb38b628b819cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621eb2d2ef75504860a05b1e8d7af763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9221389d8ec36117dec0",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "de591c1ea5603ff75e41",
+    "url": "/static/css/main.1721ec99.chunk.css"
+  },
+  {
+    "revision": "9221389d8ec36117dec0",
+    "url": "/static/js/2.f8a5ac0b.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.f8a5ac0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de591c1ea5603ff75e41",
+    "url": "/static/js/main.9df9bf16.chunk.js"
+  },
+  {
+    "revision": "f3d99727fc5a80b9d4c2",
+    "url": "/static/js/runtime-main.05c91089.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "fae3cbdfa2710f60ae85ac43f81d2125",
+    "url": "/static/media/My-logo4.fae3cbdf.ico"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/static/media/laptop1.87561258.jpg"
+  }
+]);
